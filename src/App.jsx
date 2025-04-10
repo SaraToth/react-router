f306cbf078993c618373961e67b298976fff2149
SaraToth <saratoth101@gmail.com>
2025-04-11 08:39:21 +0900

    Feat: Update App and create Profile component page
    These will be two seperate pages, that we can use to test working with react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.css'
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
