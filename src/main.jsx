a700a9987d7b840692fb59eecef1c886cef5c6f4
SaraToth <saratoth101@gmail.com>
2025-04-11 13:52:47 +0900

    Feat: Set default profile to Profile route

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Profile from './Profile.jsx'
+import DefaultProfile from './DefaultProfile.jsx'
 import Spinach from './Spinach.jsx'
 import Popeye from './Popeye.jsx'
 
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
     path: "profile",
     element: <Profile />,
     children: [
+      { index: true, element: <DefaultProfile /> },
       { path: "spinach", element: <Spinach /> },
       { path: "popeye", element: <Popeye /> },
     ]
