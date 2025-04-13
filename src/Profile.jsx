806955e8582586259eb114ac0dbd1c1aa6b0d221
SaraToth <saratoth101@gmail.com>
2025-04-14 08:43:46 +0900

    Feat: Convert profile children to dynamic render
    Use the useParams hook and converted Profile component children to be dynamically rendered instead, removing Outlet

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,13 +1,25 @@
-import { Outlet } from "react-router-dom";
+
+import DefaultProfile from "./DefaultProfile";
+import Popeye from "./Popeye";
+import Spinach from "./Spinach";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
+    const { name } = useParams();
+    console.log("name param:", name);
     return(
         <div>
             <h1>Hello from profile page!</h1>
             <p>So, how are you?</p>
             <hr />
             <h2>The profile visited is here:</h2>
-            <Outlet />
+            {name === "popeye"? (
+                <Popeye />
+            ) : name === "spinach" ? (
+                <Spinach />
+            ) : (
+                <DefaultProfile />
+            )}
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Profile from './Profile.jsx'
-import DefaultProfile from './DefaultProfile.jsx'
-import Spinach from './Spinach.jsx'
-import Popeye from './Popeye.jsx'
 
 const router = createBrowserRouter([
   {
@@ -17,11 +14,11 @@ const router = createBrowserRouter([
   {
     path: "profile",
     element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ]
+  },
+  
+  {
+    path: "/profile/:name",
+    element: <Profile />,
   },
 ]);
 
